6cf4228b236761cb26c90bb609edd60ee4900fa3
halberdssb <jwstevenson22@gmail.com>
2025-10-01 15:22:17 -0400

    Project made

---
+++ Zork/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Zork
+{
+    class Program
+    {
+        private static int LocationColumn = 1;
+        private static int LocationRow = 1;
+
+        private static readonly string[,] Rooms =
+        {
+            { "Rocky Trail", "South of House", "Canyon View" },
+            { "Forest", "West of House", "Behind House" },
+            { "Dense Woods", "North of House", "Clearing" }
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Zork!");
+
+            Commands command = Commands.UNKNOWN;
+
+            while (command != Commands.QUIT)
+            {
+                Console.Write("> ");
+                command = ToCommand(Console.ReadLine().Trim());
+
+                string outputString;
+                switch (command)
+                {
+                    case Commands.LOOK:
+                        Console.WriteLine("This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.");
+                        break;
+                    case Commands.NORTH:
+                    case Commands.SOUTH:
+                    case Commands.EAST:
+                    case Commands.WEST:
+                        Console.WriteLine("You moved " + command + ".");
+                        break;
+                    case Commands.QUIT:
+                        Console.WriteLine("Thank you for playing!");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown command.");
+                        break;
+                }
+            }
+        }
+
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+    }
+}
